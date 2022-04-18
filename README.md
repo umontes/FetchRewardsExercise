36ec4f66b049122d3dc9e8db7033c711a8968ed9
umontes <umontes@ucmerced.edu>
2022-04-18 13:56:36 -0700

    Flask admin & sqlalchemy implemented

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 In order to run this application you need to have python 3. You can download at the following link https://www.python.org/downloads/release/python-3912/
 
-1. Clone the repository into your local machine and open it.
-2. Create a virtual environment by typing "py -3 -m venv venv" in the terminal.
-3. Install Flask by typing "pip install Flask" in the terminal.
+1. Clone the repository onto your local machine.
+2. Create and activate a virtual environment.
+    * Create: py -3 -m venv venv
+    * Activate: venv\Scripts\activate
+3. Install:
+    * Flask: pip install flask
+    * flask-admin: pip install flask-admin
+    * SQL Alchemy: pip install flask-sqlalchemy
 4. 
--- app.py
+++ app.py
@@ -1,7 +1,49 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import session, sessionmaker, declarative_base
+from sqlalchemy import String, Integer, DateTime, Column, create_engine
+from sqlalchemy.sql import func
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config['FLASK_ADMIN_SWATCH'] = 'sandstone'
+app.secret_key = 'vivian'
+engine = create_engine('sqlite:///app.db', echo = True)
+db = SQLAlchemy(app)
 
+admin = Admin(app, name='Dashboard', template_mode='bootstrap3')
+
+Base = declarative_base()
+Session = sessionmaker(app)
+session = Session()
+
+# SQL Models
+class Payers(db.Model):
+    __tablename__ = 'Payers'
+    id = Column('payer_id', db.Integer, primary_key=True)
+    name = Column(db.String, unique=True)
+    points = Column(db.Integer, nullable=False)
+    timestamp = Column(db.DateTime(timezone=True), default=func.now())
+    
+    def __init__(self, name, points, timestamp):
+        self.name = name
+        self.points = points
+        self.timestamp = timestamp
+        
+db.create_all()
+db.session.commit()
+admin.add_view(ModelView(Payers, db.session))
+
+# 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/admin')
+def adminView():
+    return redirect('/admin')
+
+if __name__ == '__main__':
+    app.run()
